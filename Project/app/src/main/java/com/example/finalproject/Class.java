bc18669cbf59335020ef3c511280a6d393ea90dd
karimo123 <71343999+karimo123@users.noreply.github.com>
2021-10-21 15:31:25 -0400

    Delete Project directory

--- Project/app/src/main/java/com/example/finalproject/Class.java
+++
@@ -1,9 +0,0 @@
-package com.example.finalproject;
-
-public class Class {
-
-    public Class (String name, String description, String difficulty, int size){
-
-    }
-
-}
--- Project/app/src/main/java/com/example/finalproject/Instructor.java
+++
@@ -1,8 +0,0 @@
-package com.example.finalproject;
-
-public class Instructor extends User{
-
-    public Instructor(String firstname, String lastname, String username, String password, String email){
-        super(firstname, lastname, username, password, email);
-    }
-}
